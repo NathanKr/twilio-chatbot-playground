d26b899b687424502fa530567d3071d0c956abbb
Nathan Krasney <natankrasney@gmail.com>
2020-05-25 10:23:09 +0300

    add comment

--- email/sendGrid.js
+++ email/sendGrid.js
@@ -1,13 +1,16 @@
+const mailer = require("@sendgrid/mail");
+
 console.log("app is loading");
 
-const mailer = require("@sendgrid/mail");
+
 mailer.setApiKey(process.env.SENDGRID_API_KEY);
 const msg ={
     from : "natankrasney@gmail.com",
+    // --- the email will be sent to span if from and to are equal
     to : "theyallabeitar1@gmail.com",
-    subject : "sendGrid test",
+    subject : "sendGrid test !!!! ",
     // text : "this is the text",
-    html : "<h2>You cam also send html</h2>"
+    html : "<h2>You can also send html</h2>"
 }
 
 mailer.send(msg)
